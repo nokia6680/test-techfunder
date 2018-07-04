c302b5e50051aa02377ff17edc645f9ac5b1ac82
nokia6680 <artur.ballod@gmail.com>
2018-07-05 00:19:34 +0200

    6

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 var swiper = new Swiper('.swiper-container', {
+  centeredSlides: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
     renderBullet: function (index, className) {
       return '<span class="' + className + '">' + (index + 1) + '</span>';
     },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
